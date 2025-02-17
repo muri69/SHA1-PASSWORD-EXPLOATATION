930b468f48b7f27e92cb76ba466b20c72e4318ff
master69 <167116703+muri69@users.noreply.github.com>
2025-02-17 17:44:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import hashlib
+
+def convert_text_to_sha1(text):
+    digest = hashlib.sha1(text.encode()).hexdigest()
+    return digest
+
+def main():
+    user_sha1 = input("Enter the SHA1 to crack: ")
+    cleaned_user_sha1 = user_sha1.strip().lower()
+
+    # Deschidem fișierul cu parolele comune
+    try:
+        with open("1000-most-common-passwords.txt") as f:
+            for line in f:
+                password = line.strip()
+                converted_sha1 = convert_text_to_sha1(password)
+                
+                if cleaned_user_sha1 == converted_sha1:
+                    print(f"Password found: {password}")
+                    return  # Oprim scriptul dacă parola a fost găsită
+
+        print("Could not find the password")  # Dacă nu găsește nicio parolă corespunzătoare
+
+    except FileNotFoundError:
+        print("The file '1000-most-common-passwords.txt' was not found.")
+
+# Apelăm funcția principală pentru a rula codul
+main()
+
+
+
+     
